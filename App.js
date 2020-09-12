2111f974b8bddf02e88c1bea4b9343ac64c0d625
Adil Patel <>
2020-09-12 15:00:07 -0400

    got expo working with notifications

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+
+const askForPushPermission = (setToken) => async () => {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+  console.log("push notification status ", finalStatus);
+  if (finalStatus !== "granted") {
+    setToken(`(token ${finalStatus})`);
+  }
+  let token = await Notifications.getExpoPushTokenAsync();
+  setToken(token);
+};
 
 export default function App() {
+  const [token, setToken] = useState("(token not requested yet)");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput value={token} />
+      <Button
+        title="Ask Me for Push Permissions"
+        onPress={askForPushPermission(setToken)}
+      />
     </View>
   );
 }
@@ -14,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
